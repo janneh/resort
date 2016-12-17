6bff9d9344a9be87c1011b20ae634d5d3fafc5a9
Janne Lönn <janne@izettle.com>
2016-12-17 13:16:51 +0100

    Base Indexer implementation

--- src/indexer.js
+++ src/indexer.js
@@ -4,7 +4,31 @@ class Indexer {
     this.client = props.client
   }
 
-  index() {
+  parseWords(words) {
+    if (!words) return {}
+    const wordCounts = {}
+    for (let i = 0; i < words.length; i++) {
+      const w = words[i]
+      wordCounts[w] = wordCounts[w]
+        ? ++wordCounts[w]
+        : 1
+    }
+    return { words: Object.keys(wordCounts), wordCounts }
+  }
+
+  parseText(text) {
+    const words = text
+      .match(/\w+/g)
+      .map(w => w.toLowerCase())
+    return this.parseWords(words)
+  }
+
+  index(id, text) {
+    const { words = [], wordCounts } = this.parseText(text)
+    const cmds = words.map(word => (
+      ['zadd', `${this.key}:index:${word}`, wordCounts[word], id]
+    ))
+    this.client.multi(cmds).exec(() => {})
   }
 }
 
